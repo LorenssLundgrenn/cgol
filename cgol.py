667b4d0957be620d629e984223a553f75b2aac89
Lorenss Teo Lundgrenn <61320783+LorenssLundgrenn@users.noreply.github.com>
2022-10-28 21:53:50 +0200

    fixed crash on mouse scroll :P

--- cgol.py
+++ cgol.py
@@ -61,21 +61,18 @@ while running:
                 lifeProbability = 0
                 cells = create_grid()
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouseButtonHeld[event.button-1] = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mouseButtonHeld[event.button-1] = False
-
-    if mouseButtonHeld[0] or mouseButtonHeld[2]:
-        pos = pygame.mouse.get_pos()
-        cellY = math.floor(((pos[1]-yoffset)/tileSize)%height)
-        cellX = math.floor(((pos[0]-xoffset)/tileSize)%width)
-        if mouseButtonHeld[0]:
-            if cells[cellY][cellX] != 1:
-                cells[cellY][cellX] = 1
-        else:
-            if cells[cellY][cellX] != 0:
-                cells[cellY][cellX] = 0
+        elif event.type == (pygame.MOUSEBUTTONDOWN or pygame.MOUSEBUTTONUP):
+            mouseButtonHeld = pygame.mouse.get_pressed()
+            if mouseButtonHeld[0] or mouseButtonHeld[2]:
+                pos = pygame.mouse.get_pos()
+                cellY = math.floor(((pos[1]-yoffset)/tileSize)%height)
+                cellX = math.floor(((pos[0]-xoffset)/tileSize)%width)
+                if mouseButtonHeld[0]:
+                    if cells[cellY][cellX] != 1:
+                        cells[cellY][cellX] = 1
+                elif mouseButtonHeld[2]:
+                    if cells[cellY][cellX] != 0:
+                        cells[cellY][cellX] = 0
 
     lifeQuery = []
     deathQuery = []
